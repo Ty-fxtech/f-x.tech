e22b66eb4b0154368e4c5060a5d7e0ce7204a060
Ty-fxtech <115636194+Ty-fxtech@users.noreply.github.com>
2022-10-25 13:01:41 -0500

    Update ltinstall!.ps1

--- scripts/ltinstall!.ps1
+++ scripts/ltinstall!.ps1
@@ -6,8 +6,8 @@ param(
 if (!($InstallerToken))  {$InstallerToken = [Microsoft.VisualBasic.Interaction]::InputBox("Please paste your provided Installer Token below:","FX Tech Agent Installer")}
 if (!($InstallerToken)) {echo "Installer Token Not Entered, exiting early"; pause; exit}
 iex (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ty-fxtech/fx-ltposh/master/LabTech.psm1')
-$hexString = [System.Convert]::FromBase64String($InstallerToken + "==") 
-$hexString = ($hexString|ForEach-Object ToString X2) -join ''
+if ($InstallerToken.length -lt 32) {$hexString = [System.Convert]::FromBase64String($InstallerToken + "==") 
+                                    $InstallerToken = ($hexString|ForEach-Object ToString X2) -join '' }
 if (get-service ltservice -erroraction silentlycontinue) {Uninstall-LTService -Server "https://forrestertech.hostedrmm.com" -Force }
-Install-LTService -Server "https://forrestertech.hostedrmm.com" -InstallerToken $hexString
+Install-LTService -Server "https://forrestertech.hostedrmm.com" -InstallerToken $InstallerToken
 pause
