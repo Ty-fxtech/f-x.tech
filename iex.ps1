68290fe13e41b5ed866f8f6c1e2d676243bf9e12
Ty-fxtech <115636194+Ty-fxtech@users.noreply.github.com>
2022-10-26 11:04:51 -0500

    Update iex.ps1

--- iex.ps1
+++ iex.ps1
@@ -250,4 +250,4 @@ if ($_Uninstall) {
 ### Cleanup:
 
 $ProgressPreference = $OldProgress
-# if (!($_KeepVars)) {Get-Variable | Where-Object Name -notin $existingVariables.Name | Remove-Variable}
+if (!($_KeepVars)) {Get-Variable | Where-Object Name -notin $existingVariables.Name | Remove-Variable}
