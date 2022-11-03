c7745caa51ec23f7693e966092ca0745cb645984
Ty-fxtech <115636194+Ty-fxtech@users.noreply.github.com>
2022-11-03 08:36:47 -0500

    Update control.ps1

--- scripts/control.ps1
+++ scripts/control.ps1
@@ -1,2 +1,2 @@
 $Env:DLRemote = "https://forrestertech.hostedrmm.com:8040/Bin/ConnectWiseControl.ClientSetup.exe?h=forrestertech.hostedrmm.com&p=8041&k=BgIAAACkAABSU0ExAAgAAAEAAQDXOG5zPSNa%2B8%2B39fnb3rcd2V4wwp11r%2FCPB%2BjSxlAFhPgbqexK0%2Bt6q0KND7gDkWteBQpTBrDt3H9fPh27a0Etb%2BE3eutLOYrP755HRYPJ15a2LgUDPWkdIypuuBIvTn5juh6dTi%2FU48VNGsKiZ268IYscXrBt8F3MKqytdiWvI5wMeEE0yfgNL5u7vxkDrlfBJwjlpW6xFntEuVBTiKIz4zl1px7bic0ZtblK99gD2rBR16hZO%2FBqRoMXnW3YQn3gVuXLBUdP%2F1Y7NW53O42wlWEpnkjx8T%2BYhqJwvwkQ2%2FVq8WoEHJAY9VC2k8lyaQC4Eg0CeUBlOnnGkAruFDWr&e=Access&y=Guest&t=&c=&c=&c=&c=&c=&c=&c=&c="
-iex.run?@admin
+iex.run ?@admin
