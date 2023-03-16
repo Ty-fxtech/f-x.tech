792aefde371260f1cde30352717dea6b91978eca
Ty-fxtech <115636194+Ty-fxtech@users.noreply.github.com>
2023-03-16 12:15:33 -0500

    Update iex.ps1

--- iex.ps1
+++ iex.ps1
@@ -9,8 +9,8 @@ $OldProgress = $ProgressPreference; $ProgressPreference = "SilentlyContinue"
 
 #Display Banner
 if (!(Get-ItemProperty -erroraction ignore -Path "HKCU:\Software\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize")) {
- Set-ItemProperty -Path "HKCU:\Software\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2}
-if (!($_HideBanner)) {(((iwr -erroraction ignore "$github/customizations/banner.html").parsedhtml).body).innertext}
+ Set-ItemProperty -erroraction ignore -Path "HKCU:\Software\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2}
+if (!($_HideBanner)) {(((iwr -usebasicparsing -erroraction ignore "$github/customizations/banner.html").parsedhtml).body).innertext}
 
 # Set Default Download Folder Default location (Will be overwritten later if config exists)
 $_DownloadFolder = '$Env:Public\$github\' # Default '$Env:Public\$github\'
